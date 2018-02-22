7aae95b476af02f68838a84b3987a1301012e11e
Innoviox <sarras305@gmail.com>
2018-02-22 12:14:41 -0500

    Finished Markov.generate, fixed punctuation glitch, added support for file list

--- babblers/markov.py
+++ babblers/markov.py
@@ -1,5 +1,6 @@
 from _io import TextIOWrapper
 from string import punctuation
+import random
 
 class Markov:
     def __init__(self, text=None, n=1, start_same = False, split=' '):
@@ -7,13 +8,24 @@ class Markov:
         self.n = round(n)
         self.start_same = start_same
         self.split = split
-        self.ngrams = self._generate_ngrams()
+        self.ngrams, self.starts = self._generate_ngrams()
 
-    def generate(self):
-        pass
+    def generate(self, length=500):
+        babbled = ''
+        if self.start_same:
+            curr = [None] * self.n
+        else:
+            curr = random.choice(self.starts)
+
+        for i in range(length):
+            next = random.choice(self.ngrams[curr])
+            curr.remove(0)
+            curr.append(next)
+            babbled += next + " "
+        return babbled
 
     def _generate_ngrams(self):
-        ngrams = {}
+        ngrams, starts = {}, []
         curr = [None] * self.n
         for i in range(len(self.text) - 1):
             curr.remove(0)
@@ -21,7 +33,9 @@ class Markov:
             if curr not in ngrams:
                 ngrams[curr] = []
             ngrams[curr].append(self.text[i + 1])
-        return ngrams
+            if self.text[i] in '!.?':
+                starts.append(self.text[i+1:i+n])
+        return ngrams, starts
 
     def _parse_text(self, text):
         if text == None:
@@ -29,6 +43,8 @@ class Markov:
         elif isinstance(text, TextIOWrapper):
             with text as file:
                 _text = file.read()
+        elif isinstance(text, list):
+            pass
         elif isinstance(text, str):
             try:
                 with open(text) as file:
@@ -36,7 +52,10 @@ class Markov:
             except FileNotFoundError:
                 _text = text
         else:
-            raise TypeError("Text must be TextIOWrapper or str, not {0}".format(type(text)))
+            _text = ''
+            for file in text:
+                _text += self._parse_text(file)
+        # raise TypeError("Text must be TextIOWrapper, str, not {0}".format(type(text)))
 
         _text = _text.split(self.split)
         if self.start_same:
@@ -44,8 +63,8 @@ class Markov:
 
         parsed = []
         for word in _text:
+            parsed.append(word.strip(punctuation))
             if word.endswith(tuple(punctuation)):
-                parsed.append(word.strip(punctuation))
                 parsed.append(word.strip(word.strip(punctuation)))
         return parsed
 
